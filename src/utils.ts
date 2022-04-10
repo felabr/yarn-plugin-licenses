369022a144763de18f53b609b027efc03b7c514d
Felix Abraham <felix.abraham@sinnerschrader.com>
2022-04-10 20:32:54 +0200

    add a bunch of debug statements

--- src/utils.ts
+++ src/utils.ts
@@ -137,16 +137,34 @@ export const getSortedPackages = async (
     storedDescriptors = project.storedDescriptors.values()
   } else {
     storedDescriptors = project.workspaces.flatMap((workspace) => {
+      console.log('workspace');
+      console.log('locator', workspace.locator);
+      console.log('anchoredDescriptor', workspace.anchoredDescriptor);
+      console.log('anchoredLocator', workspace.anchoredLocator);
       const dependencies = [workspace.anchoredDescriptor]
       for (const [identHash, dependency] of workspace.dependencies.entries()) {
         if (production && workspace.manifest.devDependencies.has(identHash)) {
           continue
         }
-        dependencies.push(dependency)
+        dependencies.push(dependency);
+        if (dependency.name === 'babel-loader') {
+          console.log('babel-loader found');
+          console.log('identHash', identHash);
+          console.log('dependency', dependency);
+        }
+        if (identHash === '8377c7f4f64ffe06a1668228994fa59ba1a11017a30f97d8d045f4f352c3f84c5d34f0f4c27b081ec27424c0c8d319af10d2acf475b2e74125931254af1753b4'
+          || dependency.descriptorHash === '8377c7f4f64ffe06a1668228994fa59ba1a11017a30f97d8d045f4f352c3f84c5d34f0f4c27b081ec27424c0c8d319af10d2acf475b2e74125931254af1753b4'
+          || dependency.range.startsWith('virtual:8377c7f4f64ffe06a1668228994fa59ba1a11017a30f97d8d045f4f352c3f84c5d34f0f4c27b081ec27424c0c8d319af10d2acf475b2e74125931254af1753b4')) {
+            console.log("babel loader's yarnState hash found");
+            console.log('identHash', identHash);
+            console.log('dependency', dependency);
+          }
       }
       return dependencies
     })
   }
+
+  console.log('project storedDescriptors', storedDescriptors.filter((descriptor) => descriptor.name === 'babel-loader'));
 
   const sortedDescriptors = miscUtils.sortMap(storedDescriptors, [
     (descriptor) => structUtils.stringifyIdent(descriptor),
@@ -161,7 +179,14 @@ export const getSortedPackages = async (
     const identHash = project.storedResolutions.get(descriptor.descriptorHash)
     if (!identHash) continue
     const pkg = project.storedPackages.get(identHash)
+
     if (!pkg) continue
+
+    if (pkg.name === 'babel-loader') {
+      console.log('identHash', pkg.identHash);
+      console.log('locatorHash', pkg.locatorHash);
+      console.log('reference', pkg.reference);
+    }
 
     const { descriptorHash } = structUtils.isVirtualDescriptor(descriptor)
       ? structUtils.devirtualizeDescriptor(descriptor)
